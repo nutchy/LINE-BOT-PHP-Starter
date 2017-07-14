cc347187f37862a207da0b78a06adc8e536f5665
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-07-14 22:06:01 +0700

    //

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
         // 'text' => $text
-				'text' => rand();
+				'text' => 'hi'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
